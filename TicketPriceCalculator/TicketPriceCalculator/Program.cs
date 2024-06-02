ae8309334d2ca1a27314a5d2297c4173c567de0f
VinandoGH <vdoheneba@st.ug.edu.gh>
2024-06-02 22:02:46 +0000

    Second App Added

---
+++ TicketPriceCalculator/TicketPriceCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            int ticketPrice = (age <= 12 || age >= 65) ? 7 : 10;
+            Console.WriteLine("Ticket Price: GHC" + ticketPrice);
+        }
+    }
+}
+
